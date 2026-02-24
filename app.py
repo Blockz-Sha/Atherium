f4b3692934e79a34cccc6a8ebd76d1edf341d065
Blockz <crazelegend67@gmail.com>
2026-02-24 11:19:40 +0300

    Fix Flask port for Render deployment

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ def download():
     return send_from_directory(directory=os.path.join(app.root_path, 'static', 'downloads'),
                                path='AetheriumLauncher.exe',
                                as_attachment=True)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
