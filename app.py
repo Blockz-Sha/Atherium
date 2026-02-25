1d2a45e5af4389b1c976824dbdc3d931be4dd2a9
Blockz <crazelegend67@gmail.com>
2026-02-25 11:22:59 +0300

    Rewrite Flask app to fix download issue

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
-from flask import Flask, send_from_directory, render_template
+﻿from flask import Flask, send_from_directory, render_template
 import os
 
 app = Flask(__name__)
 
-# Home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Download route
 @app.route('/download')
 def download():
-    downloads_dir = os.path.join(app.root_path, 'static', 'downloads')
-    
-    # Get the first file in the downloads directory
-    files = os.listdir(downloads_dir)
-    if not files:
-        return "No file available for download.", 404
-
-    latest_file = files[0]  # or pick the first file
-    return send_from_directory(directory=downloads_dir, path=latest_file, as_attachment=True)
-
+    return send_from_directory(directory=os.path.join(app.root_path, 'static', 'downloads'),
+                               path='main.exe',
+                               as_attachment=True)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
